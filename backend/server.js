985ecb6a465d33aa3c0601d3cef4921cba21a895
Mittelblut9 <sauer.benedikt@outlook.de>
2021-08-04 18:56:08 +0200

    added log file create system

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 require("./server/init/file.init").fileinit(__filename, "init finished");
+
+const fs = require('fs');
+fs.mkdirSync('server/log/', { recursive: true })
+
 const express = require('express');
 
 const serverconfig = require('nconf');
@@ -6,7 +10,6 @@ const serverconfig = require('nconf');
 const app = express();
 
 serverconfig.argv().env().file({file: './server_config.json'});
-
 
 require('./server-init')(app, express);
 require('./server/route/route')(app, serverconfig)
