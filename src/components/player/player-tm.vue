2e3afefacf7a9c9bb31fe3aac7cd815f37068b08
Mittelblut9 <sauer.benedikt@outlook.de>
2021-08-10 01:36:01 +0200

    added data integration in vue component

--- src/components/player/player-tm.vue
+++ src/components/player/player-tm.vue
@@ -6,10 +6,10 @@
             </div>
             <div style="width: 400px;">
                 <div class="tab center">
-                    <span class="fsize-1-5">{{lang.words.online.up}}: <span id="p_online">12</span></span>
+                    <span class="fsize-1-5">{{lang.words.online.up}}: <span id="p_online">{{onlineplayer}}</span></span>
                 </div>
                 <div class="tab center">
-                    <span class="fsize-1-5">{{lang.words.offline.up}}: <span id="p_offline">12</span></span>
+                    <span class="fsize-1-5">{{lang.words.offline.up}}: <span id="p_offline">{{offlineplayer}}</span></span>
                 </div>
             </div>
         </header>
@@ -21,13 +21,13 @@
                     </div>
                 </a>
                 <a href="#">
-                    <div class="tab center cursor-pointer " id="showgw">
+                    <div class="tab center cursor-no-drop " id="showgw">
                         <span class="fsize-1-5">{{lang.words.gangwar}}</span>
                     </div>
                 </a>
             </div>
             <div class="tab left playerlist">
-                <table cellpadding="50">
+                <table cellpadding="50" >
                     <thead class="big">
                         <tr class="user-select-none">
                             <th>{{lang.words.offline.up}}/{{lang.words.online.up}}</th>
@@ -38,18 +38,18 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th class="online">Offline</th>
-                            <th>12345</th>
-                            <th translate="no">Test#123</th>
-                            <th>Roleplay</th>
-                            <th class="banned">banned</th>
+                        <tr v-for="item in player" @click="selectPlayer(item.pid)" :class="(selectedPid === item.pid) ? 'player_selected' : ''">
+                            <th class="user-select-none">Offline</th>
+                            <th>{{item.pid}}</th>
+                            <th translate="no" :class="(item.VIP)? 'vip' : '' ">{{item.Rkg_name}}</th>
+                            <th class="user-select-none">Roleplay</th>
+                            <th class="banned"> {{item.banned}}</th>
                         </tr>
-                        <tr>
-                            <th colspan="6">
+                        <!-- <tr>
+                            <th colspan="5">
                                 <span class="red big bold">Nix hier</span>
                             </th>
-                        </tr>
+                        </tr> -->
                     </tbody>
                 </table>
             </div>
@@ -57,21 +57,21 @@
             <div class="int_btn_menu fixed">
                 <input type="checkbox" id="open_int_menu_checkbox" class="display-none"/>
                 <div class="int_btn_main_menu">
-                    <div class="int_btn int_btn_respawn cursor-no-drop">
-                        <img src="../../assets/img/icons/repeat_respawn.png" class="right"/>
-                        <span class="tip">{{lang.words.respawn}}</span>
+                    <div :class="(selectedPid) ? 'int_btn int_btn_ts' : ' int_btn int_btn_ts cursor-no-drop'" :data-pid="selectedPid">
+                        <img src="../../assets/img/icons/repeat_respawn.png" class="right" @click="editP(selectedPid, 0)"/>
+                        <span class="tip white bold">{{lang.words.respawn}}</span>
                     </div>
-                    <div class="int_btn int_btn_ts cursor-no-drop">
-                        <img src="../../assets/img/icons/support.png" class="right"/>
-                        <span class="tip">Ts&nbsp;{{lang.words.support.up}}</span>
+                    <div :class="(selectedPid) ? 'int_btn int_btn_ts' : ' int_btn int_btn_ts cursor-no-drop'" :data-pid="selectedPid">
+                        <img src="../../assets/img/icons/support.png" class="right" @click="editP(selectedPid, 1)"/>
+                        <span class="tip white bold">Ts&nbsp;{{lang.words.support.up}}</span>
                     </div>
-                    <div class="int_btn int_btn_ban cursor-no-drop">
-                        <img src="../../assets/img/icons/banned.png" class="right"/>
-                        <span class="tip">{{lang.words.ban.more}}</span>
+                    <div :class="(selectedPid) ? 'int_btn int_btn_ts' : ' int_btn int_btn_ts cursor-no-drop'" :data-pid="selectedPid">
+                        <img src="../../assets/img/icons/banned.png" class="right" @click="editP(selectedPid, 2)"/>
+                        <span class="tip white bold">{{lang.words.ban.more}}</span>
                     </div>
-                    <div class="int_btn int_btn_info cursor-no-drop">
-                        <img src="../../assets/img/icons/more-info.png" class="right"/>
-                        <span class="tip">{{lang.words.infos.short.one.up}}</span>
+                    <div :class="(selectedPid) ? 'int_btn int_btn_ts' : ' int_btn int_btn_ts cursor-no-drop'" :data-pid="selectedPid">
+                        <img src="../../assets/img/icons/more-info.png" class="right" @click="editP(selectedPid, 3)"/>
+                        <span class="tip white bold">{{lang.words.infos.short.one.up}}</span>
                     </div>
                 </div>
                 <label for="open_int_menu_checkbox">
@@ -87,6 +87,7 @@
 <script>
 import { getLang } from '../../assets/config/txt/getLang'
 import { getConfig } from '../../assets/js/config/getConfig';
+import { player } from '../../assets/js/gameserver/player';
 
 const config = getConfig.getConfig();
 const lang = getLang();
@@ -95,8 +96,48 @@ export default {
     name: config.routing.game.player.viewplayer.name,
     data: () => {
         return {
-            lang: lang
+            lang: lang,
+            player: [],
+            pl_length: [],
+            selectedPid: '',
+            onlineplayer: '',
+            offlineplayer: '',
         }
+    },
+    methods: {
+        selectPlayer(pid) {
+            if(this.selectedPid == pid) {
+                this.selectedPid = ''
+                return;
+            }
+            this.selectedPid = pid
+        },
+        /**
+         * @param 0 = respawn
+         * @param 1 = support
+         * @param 2 = bannen
+         * @param 3 = info
+         */
+        editP(pid, type) {
+            player.editPlayer(pid, type, (response) => {
+                if(response.data) {
+                    alert(pid + ' erfolgreich geändert')
+                }
+                alert('ERROR ' + response.data)
+            });
+        }
+    },
+    async beforeCreate() {
+        await player.getPlayer((response) => {  
+            var onlineplayer = 0;
+            var offlineplayer = 0; 
+            for(let i = 0; i < response.data.length; i++ ) {    
+                this.player.push(response.data[i]);
+                (response.data[i].online) ? onlineplayer++ : offlineplayer++;
+            }
+            this.onlineplayer = onlineplayer;
+            this.offlineplayer = offlineplayer
+        });
     }
 }
 </script>
