311182c6ac5f8a513063d5d24912c12b29cadd89
Mittelblut9 <sauer.benedikt@outlook.de>
2021-09-03 22:43:10 +0200

    added application site

---
+++ src/views/Team-apply.vue
@@ -0,0 +1,41 @@
+<template>
+  <navtm :username="username" :teamid="teamid" :scname="scname" />
+  <applytm />
+</template>
+<style scoped>
+    @import url('../assets/css/apply/apply-tm.css');
+</style>
+<script>
+import navtm from '../components/_nav/nav-tm.component.vue';
+import applytm from '../components/apply/apply-tm.component.vue';
+import { getConfig } from '../assets/js/config/getConfig';
+import { getuser } from '../assets/js/getuser';
+
+const config = getConfig.getConfig();
+
+export default {
+    name: config.routing.game.apply.view.name,
+    data: () => {return {}},
+    components: {
+        navtm,
+        applytm
+    },
+    props: {
+        username: { 
+        type: String,
+        required: true,
+        default: "Max"
+        },
+        teamid: { 
+        type: Number,
+        required: true,
+        default: "99999"
+        },
+        scname: { 
+        type: String,
+        required: true,
+        default: 'MaxMuster123'
+        },
+    }
+};
+</script>
