e5b98bde6a5235cd593162dd38c31f95342515b9
Mittelblut9 <sauer.benedikt@outlook.de>
2021-09-03 22:39:57 +0200

    added application site

---
+++ src/assets/js/gameserver/apply.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { getConfig } from '../config/getConfig';
+
+const config = getConfig.getConfig();
+
+const applyGetRoute = (process.env.NODE_ENV === 'production') ? 
+                    `${process.env.VUE_APP_HTTP}${process.env.VUE_APP_BACKENDDOMAIN}/${config.routing.root.route}/api/${process.env.VUE_APP_APIV}/${config.routing.game.apply.view.route}` 
+                    : `../../api${config.routing.root.route}/${process.env.VUE_APP_APIV}${config.routing.game.apply.view.route}`;
+
+const apply = {
+    getApply: (cb) => {
+        axios.get(applyGetRoute, {
+            headers: {
+                "Content-type": "application/json",
+                'x-access-token': `${localStorage.getItem(config.keyStorageName)}`,
+                'Cache-control': "no-cache"
+            },
+            withCredentials: true
+        }).then((response) => {
+            cb(response);
+        }).catch((err) => {
+            cb(err);
+        });
+    }
+}
+
+
+export { apply };
