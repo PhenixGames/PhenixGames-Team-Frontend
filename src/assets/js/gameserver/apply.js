92b509f1dcd1341912a7eec53d901552de23bb16
Mittelblut9 <sauer.benedikt@outlook.de>
2021-09-09 21:37:16 +0200

    added a few functions to apply.js

--- src/assets/js/gameserver/apply.js
+++ src/assets/js/gameserver/apply.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
-import { getConfig } from '../config/getConfig';
+import {
+    getConfig
+} from '../config/getConfig';
 
 const config = getConfig.getConfig();
 
-const applyGetRoute = (process.env.NODE_ENV === 'production') ? 
-                    `${process.env.VUE_APP_HTTP}${process.env.VUE_APP_BACKENDDOMAIN}/${config.routing.root.route}/api/${process.env.VUE_APP_APIV}/${config.routing.game.apply.view.route}` 
-                    : `../../api${config.routing.root.route}/${process.env.VUE_APP_APIV}${config.routing.game.apply.view.route}`;
+const applyGetRoute = (process.env.NODE_ENV === 'production') ?
+    `${process.env.VUE_APP_HTTP}${process.env.VUE_APP_BACKENDDOMAIN}/${config.routing.root.route}/api/${process.env.VUE_APP_APIV}/${config.routing.game.apply.view.route}` :
+    `../../api${config.routing.root.route}/${process.env.VUE_APP_APIV}${config.routing.game.apply.view.route}`;
+
+const applyEditRoute = (process.env.NODE_ENV === 'production') ?
+    `${process.env.VUE_APP_HTTP}${process.env.VUE_APP_BACKENDDOMAIN}/${config.routing.root.route}/api/${process.env.VUE_APP_APIV}/${config.routing.game.apply.edit.route}` :
+    `../../api${config.routing.root.route}/${process.env.VUE_APP_APIV}${config.routing.game.apply.edit.route}`;
+
 
 const apply = {
     getApply: (cb) => {
@@ -21,8 +28,27 @@ const apply = {
         }).catch((err) => {
             cb(err);
         });
+    },
+    editApply: (type, bid, cb) => {
+        axios.post(applyEditRoute, {
+            bid: bid,
+            type: type
+        }, {
+            headers: {
+                "Content-type": "application/json",
+                'x-access-token': `${localStorage.getItem(config.keyStorageName)}`,
+                'Cache-control': "no-cache"
+            },
+            withCredentials: true
+        }).then((response) => {
+            cb(response);
+        }).catch((err) => {
+            cb(err);
+        });
     }
 }
 
 
-export { apply };
+export {
+    apply
+};
